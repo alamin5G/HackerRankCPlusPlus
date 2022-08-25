fe7c8d135c1f2e576965bf196d29e7995f892fbf
Md. Alamin <alaminrakib4864@gmail.com>
2022-08-25 18:37:33 +0600

    conditional statement

---
+++ introduction/conditionalstatement.cpp
@@ -0,0 +1,44 @@
+/**
+ * @file conditionalstatement.cpp
+ * https://prnt.sc/PCdLUSMFBIsu
+ * @author Md. Alamin (alamin5g@yahoo.com)
+ * I would love be a software engineer at Google. That is why anybody can uses this code without any condition, if you face any difficulty, then try to email me.
+ * @version 0.1
+ * @date 2022-08-25
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+   int n;
+   cin >> n;
+   if (n==1) {
+    cout << "one" << endl;
+   }else if (n==2) {
+    cout << "two" << endl;
+   }else if (n==3) {
+    cout << "three" << endl;
+   }else if (n==4) {
+    cout << "four" << endl;
+   }else if (n==5) {
+    cout << "five" << endl;
+   }else if (n==6) {
+    cout << "six" << endl;
+   }else if (n==7) {
+    cout << "seven" << endl;
+   }else if (n==8) {
+    cout << "eight" << endl;
+   }else if (n==9) {
+    cout << "nine" << endl;
+   }else if (n>9) {
+    cout << "Greater than 9" << endl;
+   }
+
+    return 0;
+}
