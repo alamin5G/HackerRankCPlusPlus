29bc246d08bb5b47d8a8842c6f533e66b6a4d086
Md. Alamin <alaminrakib4864@gmail.com>
2022-08-25 18:34:47 +0600

    basic data type

---
+++ introduction/basicdatatype.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file basicdatatype.cpp
+ * see the problem - https://prnt.sc/SprUm-TbOKmS
+ * @author Md. Alamin (alamin5g@yahoo.com)
+ * I would love be a software engineer at Google. That is why anybody can uses this code without any condition, if you face any difficulty, then try to email me.
+ * @version 0.1
+ * @date 2022-08-25
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <bits/stdc++.h>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin >> a >> b >> c >> d >> e;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << fixed << setprecision(3) << d << endl;
+    cout << fixed << setprecision(9) << e << endl;
+    return 0;
+}
